20a783ec949f0fe8179cbdc1e306b9e30feaa4a4
Peter-cloud-web <petergichia165@gmail.com>
2021-07-16 05:50:58 +0300

    implement BookMarkUseCase class

--- domain/src/main/java/com/seanpeters/domain/usecases/BookmarkBookUseCase.kt
+++ domain/src/main/java/com/seanpeters/domain/usecases/BookmarkBookUseCase.kt
@@ -1,4 +1,8 @@
 package com.seanpeters.domain.usecases
 
-class BookmarkBookUseCase {
+import com.seanpeters.domain.entities.Volume
+import com.seanpeters.domain.repository.BooksRepository
+
+class BookmarkBookUseCase(val booksRepository: BooksRepository){
+    suspend fun invoke(book: Volume) = booksRepository.bookmark(book)
 }
