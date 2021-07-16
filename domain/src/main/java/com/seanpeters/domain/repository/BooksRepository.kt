eba6c4e7b16ee1d75167ae761d83339a9202de45
Peter-cloud-web <petergichia165@gmail.com>
2021-07-16 05:27:31 +0300

    add interface methods to be implemented

--- domain/src/main/java/com/seanpeters/domain/repository/BooksRepository.kt
+++ domain/src/main/java/com/seanpeters/domain/repository/BooksRepository.kt
@@ -1,4 +1,9 @@
 package com.seanpeters.domain.repository
 
+import com.seanpeters.domain.entities.Volume
+import kotlinx.coroutines.flow.Flow
+
 interface BooksRepository {
+    suspend fun getRemoteBooks(author:String):Result<List<Volume>>
+    suspend fun getBookmarks(): Flow<List<Volume>>
 }
