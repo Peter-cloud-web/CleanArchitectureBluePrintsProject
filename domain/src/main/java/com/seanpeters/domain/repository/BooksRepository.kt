ac2f70ed6a6ee0c5b47f03eef569fa6b171c82e0
Peter-cloud-web <petergichia165@gmail.com>
2021-07-16 05:34:39 +0300

    Add usecase classes

--- domain/src/main/java/com/seanpeters/domain/repository/BooksRepository.kt
+++ domain/src/main/java/com/seanpeters/domain/repository/BooksRepository.kt
@@ -6,4 +6,6 @@ import kotlinx.coroutines.flow.Flow
 interface BooksRepository {
     suspend fun getRemoteBooks(author:String):Result<List<Volume>>
     suspend fun getBookmarks(): Flow<List<Volume>>
+    suspend fun bookmark(book:Volume)
+    suspend fun unbookmark(book: Volume)
 }
---
+++ domain/src/main/java/com/seanpeters/domain/usecases/UnBookMarkUseCase.kt
@@ -0,0 +1,4 @@
+package com.seanpeters.domain.usecases
+
+class UnBookMarkUseCase {
+}
